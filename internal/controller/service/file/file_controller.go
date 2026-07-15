e254546f379f580877a49217ae4108d8cd13b2c0
avelkov <andreyvelkov@live.ru>
2026-07-15 12:04:08 +0400

    Метод удаления файла и рефакторинг

--- internal/controller/service/file/file_controller.go
+++ internal/controller/service/file/file_controller.go
@@ -19,10 +19,12 @@ type FileController struct {
 func (fc *FileController) RegisterRoutes() {
 	e := fc.E
 
-	e.POST("/file:uuid", fc.FileUpload)
+	e.POST("/file/:uuid", fc.UploadFile)
+	e.GET("/file/:uuid", fc.GetFile)
+	e.DELETE("/file/:uuid", fc.DeleteFile)
 }
 
-func (fc *FileController) FileUpload(c *gin.Context) {
+func (fc *FileController) UploadFile(c *gin.Context) {
 	fileUuid := c.Param("uuid")
 
 	file, fHeader, err := c.Request.FormFile("file")
@@ -50,4 +52,47 @@ func (fc *FileController) FileUpload(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"payload": "file upload success", "filename": fHeader.Filename})
+}
+
+func (fc *FileController) GetFile(c *gin.Context) {
+	fileUuid := c.Param("uuid")
+
+	output, err := fc.DI.Client.GetObject(c.Request.Context(), &s3.GetObjectInput{
+		Bucket: aws.String(viper.GetString(chirick_config.SeaweedMainBucket)),
+		Key:    aws.String(fileUuid),
+	})
+
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "file not found"})
+		return
+	}
+	defer output.Body.Close()
+
+	contentType := "image/jpeg" // значение по умолчанию
+	if output.ContentType != nil {
+		contentType = *output.ContentType
+	}
+
+	contentLength := int64(0)
+	if output.ContentLength != nil {
+		contentLength = *output.ContentLength
+	}
+
+	c.DataFromReader(http.StatusOK, contentLength, contentType, output.Body, nil)
+}
+
+func (fc *FileController) DeleteFile(c *gin.Context) {
+	fileUuid := c.Param("uuid")
+
+	_, err := fc.DI.Client.DeleteObject(c.Request.Context(), &s3.DeleteObjectInput{
+		Bucket: aws.String(viper.GetString(chirick_config.SeaweedMainBucket)),
+		Key:    aws.String(fileUuid),
+	})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to delete file: " + err.Error()})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
 }
